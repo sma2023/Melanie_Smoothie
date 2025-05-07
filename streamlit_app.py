4350141208205086e10d5d6b6ed01f5a24bd0b76
sma2023 <125526015+sma2023@users.noreply.github.com>
2025-05-07 10:42:31 -0400

    Update streamlit_app.py
    moved lines , in for loop and beginning

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -34,6 +34,8 @@ if ingredient_list:
   ingredient_string = ''
   for fruit_chosen in ingredient_list:
      ingredient_string += fruit_chosen + ' '
+     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   #st.write(ingredient_string)
   my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
              values ('""" + ingredient_string + """', '""" + name_on_order + """')"""
@@ -45,8 +47,6 @@ if ingredient_list:
    st.success('Your Smoothie is ordered!',icon="✅")
 
 #New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
      
