0365d580a404a335165d436317edeea95996175b
sma2023 <125526015+sma2023@users.noreply.github.com>
2025-05-07 10:29:18 -0400

    Update streamlit_app.py
    added
    import requests
    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
    st.text(smoothiefroot_response)

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,16 +35,18 @@ if ingredient_list:
   for fruit_chosen in ingredient_list:
      ingredient_string += fruit_chosen + ' '
   #st.write(ingredient_string)
-
   my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
              values ('""" + ingredient_string + """', '""" + name_on_order + """')"""
  #st.write(my_insert_stmt)
  #st.stop()  
-   
-  time_to_insert = st.button('Submit Order')   
+   time_to_insert = st.button('Submit Order')   
   if time_to_insert:   
    session.sql(my_insert_stmt).collect()
    st.success('Your Smoothie is ordered!',icon="✅")
 
+#New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
      
